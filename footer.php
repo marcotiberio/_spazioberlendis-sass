2154b13a7f4154f20e6d46a769b2fdbb46990fc9
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-08-26 12:43:14 +0200

    push

--- footer.php
+++ footer.php
@@ -13,7 +13,17 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'spazioberlendis' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'spazioberlendis' ), 'WordPress' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'spazioberlendis' ), 'spazioberlendis', '<a href="https://defrostudio.eu/">Defrost</a>' );
+				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
